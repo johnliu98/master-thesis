349dddd30bd7e6ade0810e404138e5e1aa33660f
John Liu <johnliu@kth.se>
2022-03-02 10:11:31 +0100

    feat: add angle normalization functions

--- code/utils/angle.py
+++ code/utils/angle.py
@@ -1,6 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+def normalize_from(th, th_min):
+    return ((th - th_min) % (2 * np.pi)) + th_min
 
-def normalize_angle(theta):
+def zero_to_two_pi(th):
     """Normalize angle between -pi and pi"""
-    return ((theta + np.pi) % (2 * np.pi)) - np.pi
+    return normalize_from(th, 0)
+
+
+def minus_pi_to_pi(th):
+    """Normalize angle between -pi and pi"""
+    return normalize_from(th, -np.pi)
